e268aad2ab0bea9943d4202f0b75bbd01b73bb07
Bengisu Torun <bengisuhazaltorun@gmail.com>
2019-09-30 14:47:57 +0300

    Add files via upload

---
+++ deneme.m
@@ -0,0 +1,21 @@
+%imadjust degerleri 0-1 arasina ceviriyor
+a = imread('C:\Users\hakagunduz\Desktop\cicek.jpg');
+a = rgb2gray(a); %renkli bir resim ise griye cevirmeliyiz
+imhist(a);
+b = imadjust(a, [0.5,0.9],[0.3,0.4]);
+% b = imadjust(a, [0,1],[1,0]); %aslinda resmi negatife cevirmis oluyor
+figure;
+imshow(b);
+k = imadjust(a,[],[],1); %[] = default values, gama value = 1
+% k1 = imadjust(a,[],[],4);
+% k2 = imadjust(a,[],[],0.5);
+figure
+imshow(k);
+figure
+imshow(k1);
+figure
+imshow(k2);
+d = im2bw(a);
+imshow(d);
+
+ 
